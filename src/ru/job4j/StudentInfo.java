42e0bd2b32054209e8f95b9f02f223c390c18d05
Egiazar6677 <iordanyane@gmail.com>
2020-05-04 18:20:09 +0300

    ru.job4j.StudentInfo первая программа

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+      System.out.println("Egiazar Iordanyan");
+      System.out.println("02.02.1992");
+
+
+    }
+}
