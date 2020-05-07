06111f9463ed08eca5eda4675b1d49705fca60f0
Egiazar6677 <iordanyane@gmail.com>
2020-05-07 13:20:02 +0300

    ru.job4j.condition.Point

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,22 +1,22 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2,);{
-          double rsl = w;
+    public static double distance(int x1, int y1, int x2, int y2,  );{
+          double rsl = result1 + result2;
 
 
           double q = x1 - x2;
           double z = y1 - y2;
-          Math.pow(q,2);
-          Math.pow(z,2);
-          double w = q + z;
-          Math.sqrt(w);
+          double result1 = Math.pow(q,q1);
+          double result2 = Math.pow(z,z1);
+          double q1 = 2;
+          double z1 = 2;
         return rsl;
     }
     public static void main(String[] args);{
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) )" + result);
-        System.out.println(rsl);
+        System.out.println(Math.sqrt(rsl));
 
     }
 }
